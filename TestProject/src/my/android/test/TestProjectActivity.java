f697edde0ad687928351e6a7425691049322b6ac
x <DeNario@172.28.51.51>
2012-03-02 17:35:04 -0500

    I finally got it to work

---
+++ TestProject/src/my/android/test/TestProjectActivity.java
@@ -0,0 +1,18 @@
+package my.android.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+        
+        // comment
+        
+        
+        
+    }
+}
