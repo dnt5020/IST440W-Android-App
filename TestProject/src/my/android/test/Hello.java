16bd225aa719029a36f90913278a0d67da1d3fee
x <DeNario@172.28.51.51>
2012-03-02 17:31:12 -0500

    I finally got this shit to work lol

--- TestProject/src/my/android/test/Hello.java
+++ TestProject/src/my/android/test/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		//comment to see if I made a change 
+		//comment to see if I did the clone right
 	}
 
 }
