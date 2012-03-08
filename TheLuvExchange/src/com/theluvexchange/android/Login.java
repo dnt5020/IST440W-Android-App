0eb9c88e7de78da87344ceb2e85492732951d652
Niranjan Singh <nis5189@psu.edu>
2012-03-08 14:01:35 -0500

    Added the Main Menu Screen layout and changed the start-up activity to MainMenu.java from TheLuvExchange.java

---
+++ TheLuvExchange/src/com/theluvexchange/android/Login.java
@@ -0,0 +1,21 @@
+package com.theluvexchange.android;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Niranjan Singh
+ * 
+ * This is the Login activity
+ */
+public class Login extends Activity {
+	  /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        
+    }
+}
---
+++ TheLuvExchange/src/com/theluvexchange/android/MainMenu.java
@@ -0,0 +1,40 @@
+package com.theluvexchange.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+/**
+ * @author Niranjan Singh
+ * 
+ * This is the Main Menu activity. The user can click one of the several options.
+ */
+public class MainMenu extends Activity {
+	 private Activity activity = this;
+	 
+	  /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+        
+     // Button just to give a way to get to the Cities activity for testing
+        ImageButton citiesButton = (ImageButton)findViewById(R.id.imageButtonCities); 
+        
+     // Listener waits for a click on cities button
+        citiesButton.setOnClickListener(toCities);
+        
+        
+        
+    }
+    
+ // On click on cities button, opens Cities activity
+    private View.OnClickListener toCities = new View.OnClickListener() {
+		public void onClick(View view) {
+			startActivity(new Intent(activity, Cities.class));
+		}
+    };
+}
