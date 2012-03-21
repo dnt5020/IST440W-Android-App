8326ce5f786df0741a41a5dcdb396f3e3dfdefc9
SHIBANI <SHIBANI@SHIBANI-PC>
2012-03-20 23:55:34 -0700

    Added a city menu page that is opened when a specific city is selected

---
+++ TheLuvExchange/src/com/theluvexchange/android/CityMenu.java
@@ -0,0 +1,25 @@
+package com.theluvexchange.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+/**
+ * @author Shibani Chadha
+ * 
+ * This is the Main Menu activity. The user can click one of the several options.
+ * 
+ * 
+ * */
+public class CityMenu extends Activity {
+	// private Activity activity = this;
+	 
+	  /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.citymenu);
+    }
+}
